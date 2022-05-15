d841eceefbd87b3b30454d8ce4fc02d8d52a5c82
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2022-05-15 15:47:52 +0900

    update eeconfig

--- src/EeConfig.ts
+++ src/EeConfig.ts
@@ -2,7 +2,7 @@ export class EeConfigKeyboard {
   readonly override: number = 0;
   readonly enableOs: boolean = false;
   readonly enableCombo: boolean = false;
-  readonly tappingTerm10ms: number = 0;
+  readonly tappingTerm20ms: number = 0;
   readonly parserType: number = 0;
 
   constructor(init?: Partial<EeConfigKeyboard>) {
@@ -12,23 +12,24 @@ export class EeConfigKeyboard {
   public IncrementTappingTerm(): EeConfigKeyboard {
     return new EeConfigKeyboard({
       ...this,
-      tappingTerm10ms: Math.min(this.tappingTerm10ms + 10, 220),
+      tappingTerm20ms: Math.min(this.tappingTerm20ms + 20, 340),
     });
   }
 
   public DecrementTappingTerm(): EeConfigKeyboard {
     return new EeConfigKeyboard({
       ...this,
-      tappingTerm10ms: Math.max(this.tappingTerm10ms - 10, 60),
+      tappingTerm20ms: Math.max(this.tappingTerm20ms - 20, 60),
     });
   }
 
   public static Deserialize(data: number[]): EeConfigKeyboard {
+    const tapterm = (data[0] & 0xf0) >> 4;
     return new EeConfigKeyboard({
       enableOs: (data[0] & 0x01) > 0,
       override: (data[0] & 0x06) >> 1,
       enableCombo: (data[0] & 0x08) > 0,
-      tappingTerm10ms: ((data[0] & 0xf0) >> 4) * 10 + 60,
+      tappingTerm20ms: tapterm == 0 ? 200 : tapterm * 20 + 40,
       parserType: data[1] & 0x01,
     });
   }
@@ -38,10 +39,41 @@ export class EeConfigKeyboard {
       (config.enableOs ? 0x01 : 0) |
         ((config.override & 0x03) << 1) |
         (config.enableCombo ? 0x08 : 0) |
-        ((((config.tappingTerm10ms - 60) / 10) & 0x0f) << 4),
+        ((((config.tappingTerm20ms - 60) / 10) & 0x0f) << 4),
       config.parserType & 0x01,
       0,
     ];
+  }
+}
+
+export class EeConfigUser {
+  readonly mouse_gesture: number = 0;
+  constructor(init?: Partial<EeConfigUser>) {
+    Object.assign(this, init);
+  }
+
+  public IncrementMouseGesture(): EeConfigUser {
+    return new EeConfigUser({
+      ...this,
+      mouse_gesture: Math.min(this.mouse_gesture + 10, 150),
+    });
+  }
+
+  public DecrementMouseGesture(): EeConfigUser {
+    return new EeConfigUser({
+      ...this,
+      mouse_gesture: Math.max(this.mouse_gesture - 10, 10),
+    });
+  }
+  public static Deserialize(data: number[]): EeConfigUser {
+    const d = data[0] & 0x0f;
+    return new EeConfigUser({
+      mouse_gesture: d == 0 ? 50 : d * 10,
+    });
+  }
+
+  public static Serialize(config: EeConfigUser) {
+    return [(config.mouse_gesture / 10) & 0x0f, 0, 0, 0];
   }
 }
 
@@ -58,7 +90,7 @@ export class EeConfig {
   readonly stenomode: number;
   readonly handedness: number;
   readonly keyboard: EeConfigKeyboard;
-  readonly user: number[];
+  readonly user: EeConfigUser;
   readonly velocikey: number;
   readonly haptic: number[];
   readonly rgbmatrix: number[];
@@ -77,7 +109,7 @@ export class EeConfig {
     this.stenomode = d[13];
     this.handedness = d[14];
     this.keyboard = EeConfigKeyboard.Deserialize(d.slice(15, 19));
-    this.user = d.slice(19, 23);
+    this.user = EeConfigUser.Deserialize(d.slice(19, 23));
     this.velocikey = d[23];
     this.haptic = d.slice(24, 28);
     this.rgbmatrix = d.slice(28, 32);
@@ -99,7 +131,7 @@ export class EeConfig {
       config.stenomode,
       config.handedness,
       ...EeConfigKeyboard.Serialize(config.keyboard),
-      ...config.user,
+      ...EeConfigUser.Serialize(config.user),
       config.velocikey,
       ...config.haptic,
       ...config.rgbmatrix,

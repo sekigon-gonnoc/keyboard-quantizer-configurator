e355c0884064065c3c80821d98e23a1c0fb1c9b8
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2022-05-15 14:03:31 +0900

    Update

--- src/EeConfig.ts
+++ src/EeConfig.ts
@@ -3,26 +3,43 @@ export class EeConfigKeyboard {
   readonly enableOs: boolean = false;
   readonly enableCombo: boolean = false;
   readonly tappingTerm10ms: number = 0;
+  readonly parserType: number = 0;
 
-  constructor(data: number[]) {
-    if (data.length >= 1) {
-      this.enableOs = (data[0] & 0x01) > 0;
-      this.override = (data[0] & 0x06) >> 1;
-      this.enableCombo = (data[0] & 0x08) > 0;
-    }
+  constructor(init?: Partial<EeConfigKeyboard>) {
+    Object.assign(this, init);
+  }
 
-    if (data.length >= 2) {
-        this.tappingTerm10ms=data[1];
-    }
+  public IncrementTappingTerm(): EeConfigKeyboard {
+    return new EeConfigKeyboard({
+      ...this,
+      tappingTerm10ms: Math.min(this.tappingTerm10ms + 10, 220),
+    });
+  }
+
+  public DecrementTappingTerm(): EeConfigKeyboard {
+    return new EeConfigKeyboard({
+      ...this,
+      tappingTerm10ms: Math.max(this.tappingTerm10ms - 10, 60),
+    });
+  }
+
+  public static Deserialize(data: number[]): EeConfigKeyboard {
+    return new EeConfigKeyboard({
+      enableOs: (data[0] & 0x01) > 0,
+      override: (data[0] & 0x06) >> 1,
+      enableCombo: (data[0] & 0x08) > 0,
+      tappingTerm10ms: ((data[0] & 0xf0) >> 4) * 10 + 60,
+      parserType: data[1] & 0x01,
+    });
   }
 
   public static Serialize(config: EeConfigKeyboard) {
     return [
       (config.enableOs ? 0x01 : 0) |
         ((config.override & 0x03) << 1) |
-        (config.enableCombo ? 0x08 : 0),
-      config.tappingTerm10ms,
-      0,
+        (config.enableCombo ? 0x08 : 0) |
+        ((((config.tappingTerm10ms - 60) / 10) & 0x0f) << 4),
+      config.parserType & 0x01,
       0,
     ];
   }
@@ -59,7 +76,7 @@ export class EeConfig {
     this.unicode = d[12];
     this.stenomode = d[13];
     this.handedness = d[14];
-    this.keyboard = new EeConfigKeyboard(d.slice(15, 19));
+    this.keyboard = EeConfigKeyboard.Deserialize(d.slice(15, 19));
     this.user = d.slice(19, 23);
     this.velocikey = d[23];
     this.haptic = d.slice(24, 28);
--- src/EeConfigEditor.tsx
+++ src/EeConfigEditor.tsx
@@ -1,5 +1,5 @@
 import { useState, Fragment } from "react";
-import { EeConfig, EeConfigKeyboard } from "./EeConfig";
+import { EeConfig, EeConfigKeyboard, EeConfigUser } from "./EeConfig";
 
 export interface IEeconfigProps {
   config: EeConfig;
@@ -17,17 +17,17 @@ function KeyboardConfig(props: {
         <select
           name="override"
           onChange={(e) => {
-            const newConfig = {
+            const newConfig = new EeConfigKeyboard({
               ...props.config,
               override: Number(e.target.value),
-            };
+            });
             props.onChange(newConfig);
           }}
           value={props.config.override}
         >
           <option value={0}>Disable</option>
-          <option value={1}>JP</option>
-          <option value={2}>US</option>
+          <option value={1}>US keyboard on JP OS</option>
+          <option value={2}>JP keyboard on US OS</option>
         </select>
       </label>
       <label>
@@ -36,32 +36,57 @@ function KeyboardConfig(props: {
           type="checkbox"
           checked={props.config.enableCombo}
           onChange={(e) => {
-            const newConfig = {
+            const newConfig = new EeConfigKeyboard({
               ...props.config,
               enableCombo: e.target.checked,
-            };
+            });
             props.onChange(newConfig);
           }}
         ></input>
       </label>
-      <label>
-        Tapping term
-        <input
-          type="number"
-          min={0}
-          max={320}
-          step={20}
-          value={props.config.tappingTerm10ms * 10}
-          onChange={(e) => {
-            const newConfig = {
-              ...props.config,
-              tappingTerm10ms: Math.round(Number(e.target.value) / 10),
-            };
-            props.onChange(newConfig);
+      <div>
+        Tapping term: {props.config.tappingTerm20ms} ms
+        <button
+          onClick={() => {
+            props.onChange(props.config.IncrementTappingTerm());
           }}
-        ></input>
-      </label>
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            props.onChange(props.config.DecrementTappingTerm());
+          }}
+        >
+          -
+        </button>
+      </div>
     </Fragment>
+  );
+}
+
+function UserConfig(props: {
+  config: EeConfigUser;
+  onChange: (v: EeConfigUser) => void;
+}) {
+  return (
+    <div>
+      Mouse gesture: {props.config.mouse_gesture} px
+      <button
+        onClick={() => {
+          props.onChange(props.config.IncrementMouseGesture());
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          props.onChange(props.config.DecrementMouseGesture());
+        }}
+      >
+        -
+      </button>
+    </div>
   );
 }
 
@@ -84,7 +109,6 @@ export function EeConfigEditor(props: IEeconfigProps) {
           }}
         ></input>
       </label>
-      <div>Keymap:{props.config.keymap.toString()}</div>
       <KeyboardConfig
         config={props.config.keyboard}
         onChange={(v) => {
@@ -92,7 +116,13 @@ export function EeConfigEditor(props: IEeconfigProps) {
           props.onChange(newConfig);
         }}
       />
-      <div>User:{props.config.user.toString()}</div>
+      <UserConfig
+        config={props.config.user}
+        onChange={(v) => {
+          const newConfig = { ...props.config, user: v };
+          props.onChange(newConfig);
+        }}
+      />
     </Fragment>
   );
 }
